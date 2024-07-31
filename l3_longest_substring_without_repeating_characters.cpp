42512fed7f86c5a6aea061948f6180ffa49a145d
Barry Balasingham <bbalasingham@gmail.com>
2024-07-31 09:22:50 -0700

    [L3] Solutions

--- l3_longest_substring_without_repeating_characters.cpp
+++ l3_longest_substring_without_repeating_characters.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <unordered_set>
 #include <string>
 
@@ -29,6 +30,47 @@ public:
                     }
                 }
             }
+        }
+
+        return biggestWindowSize;
+    }
+
+    static int length_of_longest_substring_solution(std::string s) {
+        std::unordered_map<char, int> window;
+        int windowStartIdx = 0;
+        int biggestWindowSize = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (window[s[i]] == 0) {
+                // Not in window already
+                ++window[s[i]];
+                biggestWindowSize = std::max(biggestWindowSize, i-windowStartIdx+1);
+            } else {
+                for ( ; windowStartIdx < s.length(); ++windowStartIdx) {
+                    if (s[windowStartIdx] != s[i]) {
+                        window.erase(s[windowStartIdx]);
+                    } else {
+                        // Break because we found new start
+                        ++windowStartIdx;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return biggestWindowSize;
+    }
+
+    static int length_of_longest_substring_solution_second(std::string s) {
+        int n = s.length();
+        std::vector<int> window(128, -1);
+        int windowStartIdx = 0;
+        int biggestWindowSize = 0;
+        for (int i = 0; i < n; ++i) {
+            if (window[s[i]] >= windowStartIdx) {
+                windowStartIdx = window[s[i]] + 1;
+            }
+            window[s[i]] = i;
+            biggestWindowSize = std::max(biggestWindowSize, i - windowStartIdx + 1);
         }
 
         return biggestWindowSize;
