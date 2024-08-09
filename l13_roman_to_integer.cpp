bdc2cb7a422b25b90540f8c0abd375dd331a30ba
Barry Balasingham <bbalasingham@gmail.com>
2024-08-09 08:54:03 -0700

    [L13] Roman to integer

---
+++ l13_roman_to_integer.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class L13RomanToInteger {
+public:
+    static int roman_to_int(string s) {
+        std::unordered_map<char, int> mapping = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+        };
+        int total = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (i + 1 < s.size() && mapping[s[i]] < mapping[s[i+1]]) {
+                total += mapping[s[i+1]] - mapping[s[i]];
+                ++i;
+            } else {
+                total += mapping[s[i]];
+            }
+        }
+        return total;
+    }
+};
+
+int main() {
+    std::string word = "CMLXXVI";
+    int out = L13RomanToInteger::roman_to_int(word);
+
+    std::cout << out << std::endl;
+
+    return 0;
+}
