51d05d3701ddd25369502ba3c2bcc554192bbb56
Barry Balasingham <bbalasingham@gmail.com>
2024-08-12 09:57:05 -0700

    [L46] Valid permutation solution

--- l46_permutations.cpp
+++ l46_permutations.cpp
@@ -29,6 +29,25 @@ public:
         }
         return out;
     }
+
+    void func(vector<int>& nums, int i, vector<vector<int>>& ans) {
+        if (i == nums.size() - 1) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for (int j = i; j < nums.size(); j++) {
+            swap(nums[j], nums[i]);
+            func(nums, i + 1, ans);
+            swap(nums[i], nums[j]);
+        }
+    }
+
+    vector<vector<int>> permute_solution(vector<int>& nums) {
+        vector<vector<int>> ans;
+        func(nums, 0, ans);
+        return ans;
+    }
 };
 
 int main() {
