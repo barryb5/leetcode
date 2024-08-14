4150dfdc51d97bbd9fc8db0fd6494eaa40f07627
Barry Balasingham <bbalasingham@gmail.com>
2024-08-14 07:02:04 -0700

    [L19] Remove nth node from end of list solution

--- l19_remove_nth_node_from_end_of_list.cpp
+++ l19_remove_nth_node_from_end_of_list.cpp
@@ -46,13 +46,35 @@ public:
         delete tempNext;
         return head;
     }
+
+    static ListNode* remove_nth_from_end_solution(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* first = dummy;
+        ListNode* second = dummy;
+
+        for (int i = 0; i <= n; ++i) {
+            first = first->next;
+        }
+
+        while (first != nullptr) {
+            first = first->next;
+            second = second->next;
+        }
+
+        ListNode* temp = second->next;
+        second->next = second->next->next;
+        delete temp;
+
+        return dummy->next;
+    }
 };
 
 
 int main() {
     ListNode* node1 = new ListNode{2, nullptr};
     ListNode* head = new ListNode{1, node1};
-    ListNode* out = L19RemoveNthNodeFromEndOfList::remove_nth_from_end(head, 2);
+    ListNode* out = L19RemoveNthNodeFromEndOfList::remove_nth_from_end_solution(head, 2);
 
     cout << out->val << endl;
     // cout << out->next->val << endl;
