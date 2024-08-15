09b0dfb35393dfc0b0197954d0ba4eb5eb830684
Barry Balasingham <bbalasingham@gmail.com>
2024-08-15 06:42:18 -0700

    [L69] Sqrt Solution

--- l69_sqrt.cpp
+++ l69_sqrt.cpp
@@ -17,6 +17,27 @@ public:
 
         return sqrt;
     }
+
+    static int my_sqrt_solution(int x) {
+        if (x < 2)
+            return x;
+
+        int start = 1;
+        int end = x;
+        long mid = -1;
+
+        while (start <= end) {
+            mid = start + (end-start) / 2;
+
+            if (mid * mid > x)
+                end = mid - 1;
+            else if (mid * mid == x)
+                return mid;
+            else
+                start = mid + 1;
+        }
+        return end;
+    }
 };
 
 
