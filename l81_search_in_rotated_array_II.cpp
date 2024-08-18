7a29267e21233ea4126ba1b3b1c974495e9b7a6a
Barry Balasingham <bbalasingham@gmail.com>
2024-08-18 10:23:18 -0700

    [L81] Search in rotated array solution

--- l81_search_in_rotated_array_II.cpp
+++ l81_search_in_rotated_array_II.cpp
@@ -42,6 +42,32 @@ public:
 
         return false;
     }
+
+    static bool search_solution(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        while (low <= high) {
+            int mid = (low + high)/2;
+            if (nums[mid] == target || nums[low] == target || nums[high] == target)
+                return true;
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+            } else if (nums[low] <= nums[mid]) {
+                if (target > nums[low] && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            } else {
+                if (target > nums[mid] && target < nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return false;
+    }
 };
 
 
