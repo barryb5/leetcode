6966dabfc6b37073baddfbc46f3ec2652b2977da
Barry Balasingham <bbalasingham@gmail.com>
2024-08-23 08:05:03 -0700

    [L70] Climbing stairs solution

--- l70_climbing_stairs.cpp
+++ l70_climbing_stairs.cpp
@@ -37,6 +37,18 @@ public:
         }
         return cache[0];
     }
+    static int climbing_stairs_solution(int n) {
+        int one = 1;
+        int two = 1;
+        int temp;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            temp = one;
+            one = one + two;
+            two = temp;
+        }
+        return one;
+    }
 };
 
 
