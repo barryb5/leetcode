8c1e0bce92f1b0985d2b255e9c5cbc43c618f277
Barry Balasingham <bbalasingham@gmail.com>
2024-08-30 13:47:33 -0700

    [L343] Integer break solution

--- l343_integer_break.cpp
+++ l343_integer_break.cpp
@@ -26,6 +26,18 @@ public:
 
         return out;
     }
+
+    static int integer_break_solution(int n) {
+        int res = 1;
+        for (int i = 2; i < n; i++) {
+            int div = n / i;
+            int mod = n - i * div;
+            int v = std::pow(div+1, mod) * std::pow(div, i-mod);
+            if (v < res) return res;
+            res = v;
+        }
+        return res;
+    }
 };
 
 
